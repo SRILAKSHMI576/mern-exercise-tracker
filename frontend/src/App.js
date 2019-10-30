6d492fa16731ce51a7da6d7a2f0fe107e9feb662
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-30 15:24:42 +0530

    Created component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUsers from "./components/create-user.component";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Navbar />
       <br />
       <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUsers} />
     </Router>
   );
 }
---
+++ frontend/src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are the Exercises List Component</p>
+      </div>
+    );
+  }
+}
