9d959fb0c37b85201a41828ad1e26784667bf85b
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-11-04 15:43:03 +0530

    Create a button which is increment by value 5

--- frontend/src/ReactHooks/HookCounterTwo.js
+++ frontend/src/ReactHooks/HookCounterTwo.js
@@ -4,6 +4,12 @@ function HookCounterTwo() {
   const initialValue = 0;
 
   const [count, setCount] = useState(initialValue);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prevCount => prevCount + 1);
+    }
+  };
   return (
     <div>
       count: {count} <br />
@@ -11,6 +17,7 @@ function HookCounterTwo() {
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <br />
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={incrementFive}>Increment 5</button>
     </div>
   );
 }
