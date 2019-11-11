69a89c951ca19cd3e36fc33e974ef829eca9ec0d
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-11-11 18:40:36 +0530

    APply conditional rendering methods

--- frontend/src/ReactHooks/UserGreeting.js
+++ frontend/src/ReactHooks/UserGreeting.js
@@ -10,11 +10,28 @@ export class UserGreeting extends Component {
   }
 
   render() {
-    if (this.state.isLoggedIn) {
-      return <div>Hello Srilakshmi</div>;
-    } else {
-      return <div>Bye Srilakshmi</div>;
-    }
+    return this.state.isLoggedIn && <div>Hello Srilakshmi</div>;
+    //1........
+    // return this.state.isLoggedIn ? (
+    //   <div>Hello Srilakshmi</div>
+    // ) : (
+    //   <div>Bye Srilakshmi</div>
+    // );
+    //2..........
+    // let message;
+    // if (this.state.isLoggedIn) {
+    //   message = <div>Hello Srilakshmi</div>;
+    // } else {
+    //   message = <div>Bye Srilakshmi</div>;
+    // }
+
+    //3..........
+    // if (this.state.isLoggedIn) {
+    //   return <div>Hello Srilakshmi</div>;
+    // } else {
+    //   return <div>Bye Srilakshmi</div>;
+    // }
+    //return <div>{message}</div>;
   }
 }
 
